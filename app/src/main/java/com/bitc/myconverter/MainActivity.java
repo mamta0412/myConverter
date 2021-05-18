bdac108d5b526152938adc8715af3880c24f991e
mamta0412 <pokhrelmamta@gmail.com>
2021-05-18 09:07:51 +0545

    first commit via git

---
+++ app/src/main/java/com/bitc/myconverter/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bitc.myconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText inputFaren;
+    TextView output;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputFaren= findViewById(R.id.inputFaren);
+        output= findViewById(R.id.output);
+        button= findViewById(R.id.button);
+
+    } 
+}
